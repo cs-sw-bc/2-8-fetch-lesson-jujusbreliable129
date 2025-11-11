543d6d69f210414594eaddfa41ba4277ce20442c
jujusbreliable129 <jujusbgood@gmail.com>
2025-11-11 07:27:31 -0800

    Almost done the JS unit

--- script.js
+++ script.js
@@ -14,12 +14,13 @@ const weatherData = {
 
 // ----- Function to update DOM -----
 function displayWeather(data) {
-  document.getElementById("cityName").textContent = "Your city";
-  document.getElementById("temperature").textContent = `8°C`;
-  document.getElementById("feelslike").textContent = `6°C`;
-  document.getElementById("humidity").textContent = `90%`;
-  document.getElementById("weatherDesc").textContent = `Moderate rain`;
-  document.getElementById("weatherIcon").src = "https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0010_heavy_rain_showers.png";
+  console.log(data)
+  document.getElementById("cityName").textContent = data.location.name;
+  document.getElementById("temperature").textContent = `${data.current.temperature}°C`;
+  document.getElementById("feelslike").textContent = `${data.current.feelslike}°C`;
+  document.getElementById("humidity").textContent = `${data.current.humidity}%`;
+  document.getElementById("weatherDesc").textContent = data.current.weather_descriptions[0];
+  document.getElementById("weatherIcon").src = data.current.weather_icons[0];
 }
 
 
@@ -27,14 +28,13 @@ function displayWeather(data) {
 async function getWeather() {
   const city = document.getElementById("cityInput").value || "Mississauga";
   const errorMsg = document.getElementById("errorMsg");
-  try {
-    displayData={};
-    displayWeather(displayData);
-  } catch (error) {
-    console.error(error);
-    errorMsg.classList.remove("hidden");
+
+    fetch(`http://api.weatherstack.com/current?access_key=717854740b78b1565f435b3ce0d2eada&query=${city}`)
+    .then(response => response.json()) //unwrap the evelope and give us a JS object
+    .then(displayData => displayWeather(displayData))
+    .catch(error => console.log(error)) //call the display weather with newly received info from ap
+
   }
-}
 
 // ----- (Old way) XMLHttpRequest Example -----
 function getWeatherXMLHTTP(city) {
